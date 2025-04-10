977b8560ea0610081b9544d4b23ed8d3ef083243
qwazzarr <khmaridze@mail.ru>
2025-04-10 09:37:25 +0100

    write is nice + images

---
+++ P2Pclipboard/P2Pclipboard/ConnectedModuleView.swift
@@ -0,0 +1,8 @@
+//
+//  ConnectedModuleView.swift
+//  P2Pclipboard
+//
+//  Created by Арсений Хмара on 10.04.2025.
+//
+
+import Foundation
---
+++ P2Pclipboard/P2Pclipboard/KeyGenerator.swift
@@ -0,0 +1,8 @@
+//
+//  KeyGenerator.swift
+//  P2Pclipboard
+//
+//  Created by Арсений Хмара on 10.04.2025.
+//
+
+import Foundation
